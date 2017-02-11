b8364b7a51553c75189899b48f73cfc6f4aa6d17
maxwen <max.weninger@gmail.com>
2017-02-11 10:00:52 +0100

    All tile scroller for quickbar settings [2/2]
    - Remove the small QS tiles mod we had before
    
    Change-Id: I4cc1fe09f0ed21c8a6005e9556565e9fca764c94
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- src/com/pixeldust/settings/fragments/QuickSettings.java
+++ src/com/pixeldust/settings/fragments/QuickSettings.java
@@ -81,7 +81,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private static final String CUSTOM_HEADER_IMAGE_SHADOW = "status_bar_custom_header_shadow";
     private static final String CUSTOM_HEADER_PROVIDER = "custom_header_provider";
     private static final String CUSTOM_HEADER_BROWSE = "custom_header_browse";
-    private static final String KEY_SYSUI_QQS_COUNT = "sysui_qqs_count_key";
     private static final String PREF_QSLOCK = "lockscreen_qs_disabled";
     private static final String QS_CAT = "qs_panel";
 
@@ -94,7 +93,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private CustomSeekBarPreference mQsColumns;
     private String mDaylightHeaderProvider;
     private PreferenceScreen mHeaderBrowse;
-    private CustomSeekBarPreference mSysuiQqsCount;
     private SecureSettingSwitchPreference mQsLock;
 
     private static final int MY_USER_ID = UserHandle.myUserId();
@@ -189,12 +187,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
         mHeaderBrowse = (PreferenceScreen) findPreference(CUSTOM_HEADER_BROWSE);
         mHeaderBrowse.setEnabled(isBrowseHeaderAvailable());
-
-        mSysuiQqsCount = (CustomSeekBarPreference) findPreference(KEY_SYSUI_QQS_COUNT);
-        int SysuiQqsCount = Settings.Secure.getInt(getContentResolver(),
-                Settings.Secure.QQS_COUNT, 6);
-        mSysuiQqsCount.setValue(SysuiQqsCount / 1);
-        mSysuiQqsCount.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -246,11 +238,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                     Settings.System.STATUS_BAR_CUSTOM_HEADER_PROVIDER, value);
             int valueIndex = mHeaderProvider.findIndexOfValue(value);
             mHeaderProvider.setSummary(mHeaderProvider.getEntries()[valueIndex]);
-            return true;
-        } else if (preference == mSysuiQqsCount) {
-            int SysuiQqsCount = (Integer) newValue;
-            Settings.Secure.putInt(getActivity().getContentResolver(),
-                    Settings.Secure.QQS_COUNT, SysuiQqsCount * 1);
             return true;
         }
         return false;
