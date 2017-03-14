e44b464c017a3148f5a7adad0569ad7e9cde3bd0
Ali B <abittin@gmail.com>
2017-03-14 15:48:03 +0300

    Custom Toast color options (2/2)
     - enable changing text and icon color of toast messages
     - added change preview toast
    
    Change-Id: I7e9308b25b2baa9f338e451e968cf25ad2003805
    
    PD Edits:
    *Fix up Notification fragment.
    
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- src/com/pixeldust/settings/fragments/NotificationSettings.java
+++ src/com/pixeldust/settings/fragments/NotificationSettings.java
@@ -16,6 +16,7 @@
 
 package com.pixeldust.settings.fragments;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.UserInfo;
@@ -38,37 +39,84 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
 public class NotificationSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String KEY_TOAST_ANIMATION = "toast_animation";
+    private static final String TOAST_ICON_COLOR = "toast_icon_color";
+    private static final String TOAST_TEXT_COLOR = "toast_text_color";
 
     private ListPreference mToastAnimation;
+    private ColorPickerPreference mIconColor;
+    private ColorPickerPreference mTextColor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pixeldust_settings_notifications);
-        PreferenceScreen prefScreen = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
 
         // Toast Animations
         mToastAnimation = (ListPreference) findPreference(KEY_TOAST_ANIMATION);
         mToastAnimation.setSummary(mToastAnimation.getEntry());
         int CurrentToastAnimation = Settings.System.getInt(
-                getContentResolver(), Settings.System.TOAST_ANIMATION, 1);
+                resolver, Settings.System.TOAST_ANIMATION, 1);
         mToastAnimation.setValueIndex(CurrentToastAnimation); //set to index of default value
         mToastAnimation.setSummary(mToastAnimation.getEntries()[CurrentToastAnimation]);
         mToastAnimation.setOnPreferenceChangeListener(this);
+
+        int intColor = 0xffffffff;
+        String hexColor = String.format("#%08x", (0xffffffff & 0xffffffff));
+
+        // Toast icon color
+        mIconColor = (ColorPickerPreference) findPreference(TOAST_ICON_COLOR);
+        intColor = Settings.System.getInt(resolver,
+                Settings.System.TOAST_ICON_COLOR, 0xffffffff);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mIconColor.setNewPreviewColor(intColor);
+        mIconColor.setSummary(hexColor);
+        mIconColor.setOnPreferenceChangeListener(this);
+
+        // Toast text color
+        mTextColor = (ColorPickerPreference) findPreference(TOAST_TEXT_COLOR);
+        intColor = Settings.System.getInt(resolver,
+                Settings.System.TOAST_TEXT_COLOR, 0xffffffff);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mTextColor.setNewPreviewColor(intColor);
+        mTextColor.setSummary(hexColor);
+        mTextColor.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final ContentResolver resolver = getActivity().getContentResolver();
+
         if (preference == mToastAnimation) {
             int index = mToastAnimation.findIndexOfValue((String) newValue);
-            Settings.System.putString(getContentResolver(),
+            Settings.System.putString(resolver,
                     Settings.System.TOAST_ANIMATION, (String) newValue);
             mToastAnimation.setSummary(mToastAnimation.getEntries()[index]);
             Toast.makeText(getActivity(), mToastAnimation.getEntries()[index],
                     Toast.LENGTH_SHORT).show();
+	}  else if (preference == mIconColor) {
+            String hex = ColorPickerPreference.convertToARGB(Integer
+                   .valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(resolver,
+                   Settings.System.TOAST_ICON_COLOR, intHex);
+            Toast.makeText(getActivity(), mToastAnimation.getEntry(),
+                   Toast.LENGTH_SHORT).show();
+        } else if (preference == mTextColor) {
+            String hex = ColorPickerPreference.convertToARGB(Integer
+                  .valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(resolver,
+                  Settings.System.TOAST_TEXT_COLOR, intHex);
+            Toast.makeText(getActivity(), mToastAnimation.getEntry(),
+                  Toast.LENGTH_SHORT).show();
         }
         return false;
     }
