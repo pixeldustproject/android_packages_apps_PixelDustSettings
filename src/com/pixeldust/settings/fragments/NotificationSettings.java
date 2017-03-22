8aed67bafc1e566c84cde4f8bf45dda7f7faa952
spezi77 <spezi77@gmx.com>
2017-03-22 18:59:36 +0100

    Revert Toast animation options for now.
    Squashed:
    *Revert "Show toast animation name while testing toast"
    This reverts commit 321cb7640e8be82900044476eb120254dda2b472.
    
    *Revert "AE - XuiMod: Toast Animations [2/2]"
    This reverts commit a0bc8d670800c387f578b97f470b5ed1bc9bf0a2.
    
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- src/com/pixeldust/settings/fragments/NotificationSettings.java
+++ src/com/pixeldust/settings/fragments/NotificationSettings.java
@@ -16,61 +16,21 @@
 
 package com.pixeldust.settings.fragments;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.UserInfo;
 import android.os.Bundle;
-import android.os.UserHandle;
-import android.os.UserManager;
-import android.provider.Settings;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.ListPreference;
-import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
-import android.support.v7.preference.Preference.OnPreferenceChangeListener;
-import android.widget.Toast;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
+
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.List;
-
-public class NotificationSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
-
-    private static final String KEY_TOAST_ANIMATION = "toast_animation";
-
-    private ListPreference mToastAnimation;
+public class NotificationSettings extends SettingsPreferenceFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pixeldust_settings_notifications);
         PreferenceScreen prefScreen = getPreferenceScreen();
-
-        // Toast Animations
-        mToastAnimation = (ListPreference) findPreference(KEY_TOAST_ANIMATION);
-        mToastAnimation.setSummary(mToastAnimation.getEntry());
-        int CurrentToastAnimation = Settings.System.getInt(
-                getContentResolver(), Settings.System.TOAST_ANIMATION, 1);
-        mToastAnimation.setValueIndex(CurrentToastAnimation); //set to index of default value
-        mToastAnimation.setSummary(mToastAnimation.getEntries()[CurrentToastAnimation]);
-        mToastAnimation.setOnPreferenceChangeListener(this);
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mToastAnimation) {
-            int index = mToastAnimation.findIndexOfValue((String) newValue);
-            Settings.System.putString(getContentResolver(),
-                    Settings.System.TOAST_ANIMATION, (String) newValue);
-            mToastAnimation.setSummary(mToastAnimation.getEntries()[index]);
-            Toast.makeText(getActivity(), mToastAnimation.getEntries()[index],
-                    Toast.LENGTH_SHORT).show();
-        }
-        return false;
     }
 
     @Override
