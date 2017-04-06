e33b8bd7acb9d1c18ca59a47e9c88b6b750fd55b
spezi77 <spezi77@gmx.com>
2017-04-06 20:29:49 +0200

    Slim recents panel: really default to a neutral background color.
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- src/com/pixeldust/settings/fragments/SlimRecentPanel.java
+++ src/com/pixeldust/settings/fragments/SlimRecentPanel.java
@@ -203,7 +203,7 @@ public class SlimRecentPanel extends SettingsPreferenceFragment implements
         } else if (preference == mRecentPanelBgColor) {
             String hex = ColorPickerPreference.convertToARGB(
                     Integer.valueOf(String.valueOf(newValue)));
-            if (hex.equals("#763367d6")) {
+            if (hex.equals("#00ffffff")) {
                 preference.setSummary(R.string.default_string);
             } else {
                 preference.setSummary(hex);
@@ -292,9 +292,9 @@ public class SlimRecentPanel extends SettingsPreferenceFragment implements
                 (ColorPickerPreference) findPreference(RECENT_PANEL_BG_COLOR);
         mRecentPanelBgColor.setOnPreferenceChangeListener(this);
         final int intColor = Settings.System.getInt(getContext().getContentResolver(),
-                Settings.System.RECENT_PANEL_BG_COLOR, 0x763367d6);
+                Settings.System.RECENT_PANEL_BG_COLOR, 0x00ffffff);
         String hexColor = String.format("#%08x", (0x00ffffff & intColor));
-        if (hexColor.equals("#763367d6")) {
+        if (hexColor.equals("#00ffffff")) {
             mRecentPanelBgColor.setSummary(R.string.default_string);
         } else {
             mRecentPanelBgColor.setSummary(hexColor);
