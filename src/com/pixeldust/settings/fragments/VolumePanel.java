3f7f09a554ded4c9a65e5ae2cafb54e37f724eac
ezio84 <brabus84@gmail.com>
2017-05-11 12:44:44 +0200

    Launch music player on headset connect: add BT compatibility [2/2]
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- src/com/pixeldust/settings/fragments/VolumePanel.java
+++ src/com/pixeldust/settings/fragments/VolumePanel.java
@@ -16,20 +16,58 @@
 
 package com.pixeldust.settings.fragments;
 
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.os.UserHandle;
+
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.support.v14.preference.SwitchPreference;
+
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
-import android.os.Bundle;
 import com.android.settings.R;
-
 import com.android.settings.SettingsPreferenceFragment;
 
-public class VolumePanel extends SettingsPreferenceFragment {
+public class VolumePanel extends SettingsPreferenceFragment implements
+         Preference.OnPreferenceChangeListener {
+
+    private static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
+
+    private ListPreference mLaunchPlayerHeadsetConnection;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.pixeldust_settings_volume);
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mLaunchPlayerHeadsetConnection = (ListPreference) findPreference(HEADSET_CONNECT_PLAYER);
+        int mLaunchPlayerHeadsetConnectionValue = Settings.System.getIntForUser(resolver,
+                Settings.System.HEADSET_CONNECT_PLAYER, 0, UserHandle.USER_CURRENT);
+        mLaunchPlayerHeadsetConnection.setValue(Integer.toString(mLaunchPlayerHeadsetConnectionValue));
+        mLaunchPlayerHeadsetConnection.setSummary(mLaunchPlayerHeadsetConnection.getEntry());
+        mLaunchPlayerHeadsetConnection.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        if (preference == mLaunchPlayerHeadsetConnection) {
+            int mLaunchPlayerHeadsetConnectionValue = Integer.valueOf((String) newValue);
+            int index = mLaunchPlayerHeadsetConnection.findIndexOfValue((String) newValue);
+            mLaunchPlayerHeadsetConnection.setSummary(
+                    mLaunchPlayerHeadsetConnection.getEntries()[index]);
+            Settings.System.putIntForUser(resolver, Settings.System.HEADSET_CONNECT_PLAYER,
+                    mLaunchPlayerHeadsetConnectionValue, UserHandle.USER_CURRENT);
+            return true;
+        }
+        return false;
     }
 
     @Override
